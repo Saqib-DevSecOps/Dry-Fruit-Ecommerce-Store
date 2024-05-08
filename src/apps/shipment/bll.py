21a1668dc7ca1a211b9f320ded68d575d5952a36
saqib-devops <saqibahmad778866@gmail.com>
2024-05-09 01:30:47 +0500

    Issues:Issues Fixed on website

--- src/apps/shipment/bll.py
+++ src/apps/shipment/bll.py
@@ -1,16 +1,24 @@
+api_user_email = "saqibahmad77866@gmail.com"
+api_user_password = "Admin@123"
+
 import requests
 import json
 
 url = "https://apiv2.shiprocket.in/v1/external/auth/login"
 
 payload = json.dumps({
-  "email": "saqib71501@gmail.com",
-  "password": "saqib71501@gmail.com"
+    "email": api_user_email,
+    "password": api_user_password
 })
 headers = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
-
-print(response.text)
+try:
+    response = requests.post(url, headers=headers, data=payload)
+    response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    token = response.json()["token"]
+    print("Token:", token)
+except requests.exceptions.RequestException as e:
+    print("Request failed:", e)
+    print("Response content:", response.content)
