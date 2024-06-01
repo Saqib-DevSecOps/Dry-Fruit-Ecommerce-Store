4b28174d7b55383d38b5fcb071bb31eddeb55c4b
saqib-devops <saqibahmad778866@gmail.com>
2024-06-01 20:52:59 +0500

    Bll Update ANd Issue Fixed

--- src/administration/admins/bll.py
+++ src/administration/admins/bll.py
@@ -120,7 +120,10 @@ def get_custom_shipping_charge(cart_items, service_type, state):
     matched_state = match_state(state_patterns, state)
 
     for cart_item in cart_items:
-        actual_weight = cart_item.product_weight.get_product_size()
+        if cart_item.product_weight is not None:
+            actual_weight = cart_item.product_weight.get_product_size()
+        else:
+            actual_weight = 0
         custom_shipping_cost += calculate_custom_shipping_cost(actual_weight.weight, service_type, matched_state)
     return custom_shipping_cost
 
