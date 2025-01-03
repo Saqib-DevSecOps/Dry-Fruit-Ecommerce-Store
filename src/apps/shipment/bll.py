819f3cc699c827395d61c2b12af4b07693878030
saqib-devops <saqibahmad778866@gmail.com>
2025-01-03 20:37:00 +0500

    Shipment api sku number limit assign

--- src/apps/shipment/bll.py
+++ src/apps/shipment/bll.py
@@ -86,7 +86,7 @@ def create_shiprocket_order(form, order):
         "order_items": [
             {
                 "name": item.product.title,
-                "sku": f"{item.product.sku}_{item.product.pk}_{counter}"[:50],
+                "sku": f"{counter}_{item.id}_{item.product.sku}_{item.product.pk}_"[:49],
                 "units": item.qty,
                 "selling_price": str(item.product.get_price()),
             }
